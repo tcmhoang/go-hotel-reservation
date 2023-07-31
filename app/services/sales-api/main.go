266460f08e79a404f8d67695e93da38b4a328d5a
Conrad Hoang <tcmhoang@outlook.com>
2023-07-31 05:38:14 -0400

    Init logger

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -9,23 +8,58 @@ import (
 	"syscall"
 
 	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var build = "develop"
 
 func main() {
+	logger, err := initLogger()
+	if err != nil {
+		log.Printf("Error constructing logger: %s", err)
+		os.Exit(1)
+	}
+	defer logger.Sync()
+
+	if err := run(logger); err != nil {
+		logger.Errorw("Startup", "ERROR", err)
+		os.Exit(1)
+	}
+
+}
+
+func run(log *zap.SugaredLogger) error {
 	if _, err := maxprocs.Set(); err != nil {
-		fmt.Printf("maxprocs: %s", err)
+		log.Errorw("maxprocs", "ERROR", err)
 		os.Exit(1)
 	}
 
 	g := runtime.GOMAXPROCS(0)
-	log.Printf("Starting service build[%s] CPU[%d]", build, g)
-	defer log.Println("Service ended")
+	log.Infof("Starting service build[%s] CPU[%d]\n", build, g)
+	defer log.Infoln("Service ended")
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 	<-shutdown
 
-	log.Println("Stopping service")
+	log.Infoln("Stopping service")
+	return nil
+}
+
+func initLogger() (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": "SALES-API",
+	}
+
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/tcmhoang/sservices
 
 go 1.20
 
-require go.uber.org/automaxprocs v1.5.3
+require (
+	go.uber.org/automaxprocs v1.5.3
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
