315fed323a2967d82f78464232a23d37d37386e1
Conrad Hoang <tcmhoang@outlook.com>
2023-08-03 11:55:08 -0400

    implement customm errors

---
+++ business/sys/validation/validation.go
@@ -0,0 +1,2 @@
+// Support for validating nodels
+package validation
---
+++ foundation/web/shutdown.go
@@ -0,0 +1,20 @@
+package web
+
+import "errors"
+
+type shutdownError struct {
+	Message string
+}
+
+func NewShutdownError(message string) error {
+	return &shutdownError{message}
+}
+
+func (se *shutdownError) Error() string {
+	return se.Message
+}
+
+func IsShutdownErr(err error) bool {
+	var se *shutdownError
+	return errors.As(err, &se)
+}
