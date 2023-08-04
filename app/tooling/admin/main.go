866a007b43077fd930104a1d264f6120ec12bab2
Conrad Hoang <tcmhoang@outlook.com>
2023-08-04 07:01:48 -0400

    implement admin tool creating priv and pub rsa keys

---
+++ app/tooling/admin/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
+)
+
+func main() {
+	err := genKey()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func genKey() error {
+	privkey, err := rsa.GenerateKey(rand.Reader, 2048)
+
+	if err != nil {
+		return err
+	}
+
+	privfile, err := os.Create("private.pem")
+	if err != nil {
+		return fmt.Errorf("creating private file: %w", err)
+	}
+	defer privfile.Close()
+
+	privblk := pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privkey),
+	}
+
+	if err := pem.Encode(privfile, &privblk); err != nil {
+		return fmt.Errorf("encoding to private file: %w", err)
+	}
+
+	asn1Bs, err := x509.MarshalPKIXPublicKey(&privkey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	pubfile, err := os.Create("public.pem")
+	if err != nil {
+		return fmt.Errorf("creating public file: %w", err)
+	}
+	defer pubfile.Close()
+
+	pubblk := pem.Block{
+		Type:  "RSA PUBLICKEY",
+		Bytes: asn1Bs,
+	}
+
+	if err := pem.Encode(pubfile, &pubblk); err != nil {
+		return fmt.Errorf("Encoding to public file: %w", err)
+	}
+
+	return nil
+}
