3bc78152b4c26e9a57ddf8b4734cbd4b7711da8d
Conrad Hoang <tcmhoang@outlook.com>
2023-08-04 23:49:48 -0400

    hack token generation

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -6,16 +6,82 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"io"
 	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func main() {
-	err := genKey()
+	err := genToken()
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func genToken() error {
+	name := "zarf/keys/private.pem"
+	file, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+
+	privpem, err := io.ReadAll(io.LimitReader(file, 1024*1024))
+	if err != nil {
+		return fmt.Errorf("reading auth private key: %w", err)
+	}
+
+	privkey, err := jwt.ParseRSAPrivateKeyFromPEM(privpem)
+	if err != nil {
+		return fmt.Errorf("parsing auth private key: %w", err)
+	}
+
+	claims := struct {
+		jwt.RegisteredClaims
+		Roles []string
+	}{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "service project",
+			Subject:   "123456789",
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(8760 * time.Hour).UTC()),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod("RS256")
+	token := jwt.NewWithClaims(method, claims)
+	token.Header["kid"] = "123"
+
+	str, err := token.SignedString(privkey)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("TOKEN BEGIN")
+	fmt.Println(str)
+	fmt.Println("TOKEN END")
+
+	fmt.Println()
+
+	asn1Bs, err := x509.MarshalPKIXPublicKey(&privkey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	pubblk := pem.Block{
+		Type:  "RSA PUBLICKEY",
+		Bytes: asn1Bs,
+	}
+
+	if err := pem.Encode(os.Stdout, &pubblk); err != nil {
+		return fmt.Errorf("Encoding to public file: %w", err)
+	}
+
+	return nil
 }
 
 func genKey() error {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/ardanlabs/conf v1.5.0
 	github.com/dimfeld/httptreemux/v5 v5.5.0
+	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/google/uuid v1.3.0
 	go.uber.org/automaxprocs v1.5.3
 	go.uber.org/zap v1.24.0
