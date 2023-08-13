c2a9aa3299f9bf4726b73f60cb199d70ca76484e
Conrad Hoang <tcmhoang@outlook.com>
2023-08-13 09:46:21 -0400

    implement validation basic functions

--- business/sys/validation/validation.go
+++ business/sys/validation/validation.go
@@ -1,2 +1,54 @@
 // Package validation supports for validating nodels
 package validation
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
+)
+
+var validate *validator.Validate
+
+var translator ut.Translator
+
+func init() {
+	validate := validator.New()
+	translator, _ = ut.New(en.New(), en.New()).GetTranslator("en")
+
+	en_translations.RegisterDefaultTranslations(validate, translator)
+
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+}
+
+func Check(val any) error {
+	if err := validate.Struct(val); err != nil {
+		verrors, ok := err.(validator.ValidationErrors)
+		if !ok {
+			return err
+		}
+
+		var ferrs FieldErrors
+
+		for _, verr := range verrors {
+			ferr := FieldError{
+				Field: verr.Field(),
+				Error: verr.Error(),
+			}
+
+			ferrs = append(ferrs, ferr)
+		}
+		return ferrs
+
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/ardanlabs/conf v1.5.0
 	github.com/ardanlabs/darwin/v3 v3.3.0
 	github.com/dimfeld/httptreemux/v5 v5.5.0
+	github.com/go-playground/locales v0.14.1
+	github.com/go-playground/universal-translator v0.18.1
+	github.com/go-playground/validator/v10 v10.15.0
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/google/uuid v1.3.0
 	github.com/jackc/pgx/v5 v5.4.3
@@ -15,10 +18,14 @@ require (
 )
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 )
